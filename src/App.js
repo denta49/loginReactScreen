cf5ad9900fd87fbdb25083c8cf1f188f779bee85
przemekWojenka <pdentaw@gmail.com>
2022-07-06 11:47:18 +0200

    add contex, provider, consumer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContex from "./contex/auth-contex";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,13 +31,15 @@ function App() {
     };
 
     return (
-        <React.Fragment>
+        <AuthContex.Provider value={{
+            isLoggedIn: isLoggedIn
+        }} >
             <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
             <main>
                 {!isLoggedIn && <Login onLogin={loginHandler}/>}
                 {isLoggedIn && <Home onLogout={logoutHandler}/>}
             </main>
-        </React.Fragment>
+        </AuthContex.Provider>
     );
 }
 
